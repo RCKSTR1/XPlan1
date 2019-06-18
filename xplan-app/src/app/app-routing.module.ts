659f3d07e92716f08f804fc43e956a9bdadfcb7e
Adrian Gonzalez <rckstr@outlook.com>
2019-06-17 22:58:40 -0700

    Updated routes and nav component

--- xplan-app/src/app/app-routing.module.ts
+++ xplan-app/src/app/app-routing.module.ts
@@ -1,7 +1,15 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { DashboardComponent } from './components/dashboard/dashboard.component';
+import { IngresosComponent } from './components/ingresos/ingresos.component';
+import { GastosComponent } from './components/gastos/gastos.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', redirectTo: 'dashboard', pathMatch: 'full' },
+  { path: 'dashboard', component: DashboardComponent },
+  { path: 'ingresos', component: IngresosComponent },
+  { path: 'gastos', component: GastosComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
