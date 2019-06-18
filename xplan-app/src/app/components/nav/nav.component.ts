06ae286d9d87486d45b2a540bb137eec2e969423
Adrian Gonzalez <rckstr@outlook.com>
2019-06-17 23:10:58 -0700

    Navigation complete

--- xplan-app/src/app/components/nav/nav.component.ts
+++ xplan-app/src/app/components/nav/nav.component.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 import { BreakpointObserver, Breakpoints } from '@angular/cdk/layout';
 import { Observable } from 'rxjs';
 import { map } from 'rxjs/operators';
+import { NavMenuItem } from 'src/app/models/models';
 
 @Component({
   selector: 'app-nav',
@@ -9,12 +10,17 @@ import { map } from 'rxjs/operators';
   styleUrls: ['./nav.component.css']
 })
 export class NavComponent {
+  menu = [
+    { Label: 'Dashboard', Target: '/dashboard' },
+    { Label: 'Ingresos', Target: '/ingresos' },
+    { Label: 'Gastos', Target: '/gastos' }
+  ] as NavMenuItem[];
 
   isHandset$: Observable<boolean> = this.breakpointObserver.observe(Breakpoints.Handset)
     .pipe(
       map(result => result.matches)
     );
 
-  constructor(private breakpointObserver: BreakpointObserver) {}
+  constructor(private breakpointObserver: BreakpointObserver) { }
 
 }
