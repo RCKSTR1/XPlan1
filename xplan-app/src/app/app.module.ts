e44ff85e074dd467eb4594bf4696e5133abcb595
Adrian Gonzalez <rckstr@outlook.com>
2019-06-17 22:50:02 -0700

    Added material and required components

--- xplan-app/src/app/app.module.ts
+++ xplan-app/src/app/app.module.ts
@@ -3,14 +3,28 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { NavComponent } from './components/nav/nav.component';
+import { LayoutModule } from '@angular/cdk/layout';
+import { MaterialModule } from './modules/material/material.module';
+import { DashboardComponent } from './components/dashboard/dashboard.component';
+import { IngresosComponent } from './components/ingresos/ingresos.component';
+import { GastosComponent } from './components/gastos/gastos.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavComponent,
+    DashboardComponent,
+    IngresosComponent,
+    GastosComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    LayoutModule,
+    MaterialModule
   ],
   providers: [],
   bootstrap: [AppComponent]
